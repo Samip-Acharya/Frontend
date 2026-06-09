7fc4618019a0017f98ad7cd8f2812016eb028efd
Samip-Acharya <bygkttan90level@gmailcom>
2026-06-09 23:04:59 +0545

    Scroll Trigger

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+var tl = gsap.timeline({
+    scrollTrigger: {
+        trigger:"#wrapper",
+        start:"top 60%",
+        end:"top -100%",
+        // markers:true,
+        scrub:true
+    }
+})
+
+tl.from(".box",{
+    x:-500,
+    stagger:0.4,
+})
