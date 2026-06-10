3d7a9d0ce4e63f708997679de31accb60279e611
Samip-Acharya <bygkttan90level@gmailcom>
2026-06-11 02:32:27 +0545

    navbar

--- React/Basics/src/App.jsx
+++ React/Basics/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import Navbar from './components/navbar'
 
 function App() {
 
   return (
     <>
-      <h1>Hello React</h1>
-      <div id="circle"></div>
+    <Navbar/> 
+    <Navbar/> 
+    <Navbar/> 
     </>
   )
 }
---
+++ React/Basics/src/components/navbar.jsx
@@ -0,0 +1,16 @@
+
+function Navbar(){
+    return(
+        <nav>
+            <h1><span>R</span>eact</h1>
+            <div id="nav-part-2">
+                <div><h3>Home</h3></div>
+                 <div><h3>About</h3></div>
+                  <div><h3>Service</h3></div>
+                   <div><h3>Contact</h3></div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar 
