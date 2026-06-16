52545433cbb8d74dc82dc69a50439e65949c306a
Samip-Acharya <bygkttan90level@gmail.com>
2026-06-16 19:28:48 +0545

    tailwind

---
+++ React/tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from './compnents//button/Button.jsx'
+import Header from './compnents/header/Header.jsx'
+const App = () => {
+  return (
+    <>
+      <Button></Button>
+      <Header></Header>
+    </>
+  )
+}
+
+export default App
---
+++ React/tailwind/src/compnents/button/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './Button.module.css'
+const Button = () => {
+  return (
+    <div className="text-3xl underline">button</div>
+  )
+}
+
+export default Button
